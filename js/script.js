314ea7b3a155630dd72283779feddbd78b08d55a
Kristopher Rollert <krollert@ucsc.edu>
2018-10-03 21:21:49 -0700

    added full site:

--- js/script.js
+++ js/script.js
@@ -4,4 +4,28 @@ $(document).ready(function () {
     }, function () {
         $(".spinner, .filler, .mask, .inner-image").css("animation-play-state", "running");
     });
+
+    $(".project-sections > li").click(function (e) {
+        if (e.currentTarget.classList[0] != 'project-selected') {
+            $('.project-selected').removeClass('project-selected');
+            switch (e.currentTarget.id) {
+                case 'all-tab':
+                    $('#all-tab').addClass('project-selected');
+                    $('.programming-section').show();
+                    $('.web-section').show();
+                    break;
+                case 'web-tab':
+                    $('#web-tab').addClass('project-selected');
+                    $('.programming-section').hide();
+                    $('.web-section').show()
+                    break;
+                case 'prog-tab':
+                    $('#prog-tab').addClass('project-selected');
+                    $('.programming-section').show();
+                    $('.web-section').hide()
+                    break;
+            }
+        }
+    });
+
 });
